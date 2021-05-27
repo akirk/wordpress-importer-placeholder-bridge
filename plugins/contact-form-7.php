dbad0fe2fbfc29aca6702b126f836cf1143030fd
Alex Kirk <akirk@users.noreply.github.com>
2021-05-27 22:10:14 +0200

    Add a contact form 7 bridge

---
+++ plugins/contact-form-7.php
@@ -0,0 +1,40 @@
+<?php
+add_filter( 'wp_import_object_placeholder', function( $placeholder, $type, $data ) {
+	if (
+		'http://wordpress.org/export/objects/contact-form/1.0/' !== $type
+		|| ! isset( $data['fields'] )
+	) {
+		return $placeholder;
+	}
+
+	// Create a contact form from the data:
+	$form = array();
+	foreach ( $data['fields'] as $field ) {
+		if ( 'submit' === $field['type'] ) {
+			$form[] = '[submit "' . esc_html( $field['label'] ) . '"]';
+			continue;
+		}
+		$entry = '<label>' . esc_html( $field['label'] ) . PHP_EOL;
+		$entry .= '[' . $field['type'];
+		if ( 'true' === $field['required'] || 'false' !== $field['required'] ) {
+			$entry .= '*';
+		}
+		$entry .= ' ' . sanitize_title( $field['label'] );
+		$form[] = $entry . ']</label>';
+	}
+
+
+	$contact_form = wpcf7_save_contact_form( array(
+		'form' => implode( PHP_EOL, $form ),
+		'mail' => array(
+			'recipient' => $data['email']
+		),
+	) );
+	$contact_form_id = $contact_form->id;
+
+	return '<!-- wp:contact-form-7/contact-form-selector {"id":' . $contact_form_id . ',"title":"Contact"} -->
+<div class="wp-block-contact-form-7-contact-form-selector">[contact-form-7 id="' . $contact_form_id . '" title="Contact"]</div>
+<!-- /wp:contact-form-7/contact-form-selector -->';
+
+}, 10, 3 );
+
---
+++ wordpress-importer-placeholder-bridge.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: WordPress Importer Placeholder Bridge
+ * Version: 0.1
+ *
+ * Description: Here we collect some
+ *
+ */
+
+foreach ( get_option( 'active_plugins' ) as $plugin_file ) {
+	if ( 'contact-form-7/wp-contact-form-7.php' === $plugin_file ) {
+		include __DIR__ . '/plugins/contact-form-7.php';
+	}
+}
